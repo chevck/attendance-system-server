d85634d88814abae4f75a89518a863c8e2ac1140
Excellence D. <eoyeniran@tradedepot.co>
2024-08-07 14:52:58 +0100

    fix: save body

--- models/attendance.model.js
+++ models/attendance.model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const AttendanceSchema = new Schema({
   email: { type: String },
   signedIn: { type: Date, default: new Date() },
+  name: { type: String },
 });
 
 module.exports = mongoose.model("attendanceList", AttendanceSchema);
